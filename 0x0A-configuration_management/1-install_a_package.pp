57df7abe9113df22c91c5f12239b2187f1628f18
mojtabababiker <mojmohammad98@gmail.com>
2023-11-27 06:23:01 +0200

    Edit task 1, fixed wrong versions, project 0x03

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,14 @@
 # install flask from pip3 using puppet
 
+package {'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'Werkzeug',
+  provider => pip3
+  }
+
 package {'Flask':
   ensure   => '2.1.0',
   name     => 'Flask',
-  provider => pip3
+  provider => pip3,
+  require  => Package['werkzeug']
   }
