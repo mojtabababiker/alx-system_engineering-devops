0d7b498b4b7909b14ab7a98c57dc38702b087ce4
mojtabababiker <mojmohammad98@gmail.com>
2024-03-05 20:44:29 +0200

    add task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,6 +3,7 @@
 python model that hold task 3
 """
 import requests
+from operator import itemgetter
 
 
 def count_words(subreddit, word_list, word_dict={}, after=''):
@@ -12,7 +13,7 @@ def count_words(subreddit, word_list, word_dict={}, after=''):
     """
 
     url = "https://www.reddit.com/"
-    path = f"r/{subreddit}/hot.json?after={after}&count=1"
+    path = f"r/{subreddit}/hot.json?count=1&limit=1&after={after}"
     headers = {"User-Agent": "ALX/1"}
 
     res = requests.get(url+path, headers=headers, allow_redirects=False)
@@ -21,8 +22,22 @@ def count_words(subreddit, word_list, word_dict={}, after=''):
         try:
             post = res.json()['data']['children'][0]
             title = post['data']['title']
-                for word in word_list:
-                    if word.lower() in title.lower():
-                        word_dict[word.lower()] += word_dict.get(word.lower(), 0) + 1
+            title = title.lower().split()
+            for word in word_list:
+                if word.lower() in title:
+                    word_dict[word.lower()] += word_dict.get(word.lower(), 0) + 1
             after = post['data']['name']
+            try:
+                sorted_dict = dict(sorted(word_dict.items(), key=itemgetter(0, 1)))
+            except Exception as e:
+                sorted_dict = word_dict
+            word_dict = count_words(subreddit, word_list, word_dict=sorted_dict, after=after)
+        except Exception as e:
+            # pass
 
+    else:
+        #count_words(subreddit, word_list, word_dict=word_dict, after=after)
+        return word_dict
+    for key, val in word_dict.items():
+        print("{}: {}".format(key, value))
+    #word_dict = {}
