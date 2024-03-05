3c5835309d5153ec170a796063284105e66e6b82
mojtabababiker <mojmohammad98@gmail.com>
2024-03-05 19:34:11 +0200

    Edit task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,17 +13,20 @@ def top_ten(subreddit):
     subreddit: str, text represents the subreddit title
     """
     url = "https://www.reddit.com/"
-    path = f"r/{subreddit}/hot.json?count=10"
-    headers = {"User-Agent": "ALX/1"}
+    path = "r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "ALX/1", "Accept": "application/json"}
     posts_titles = []
 
     if subreddit is None:
         print(None)
         return
     res = requests.get(url+path, headers=headers, allow_redirects=False)
+
+    if res.raise_for_status():
+        print(None)
+        return
     if res.status_code == 200:
         try:
-            # pprint.pprint(res.json())
             post_list = res.json()['data']['children']
             for idx, post in enumerate(post_list):
                 if idx >= 10:
@@ -31,10 +34,11 @@ def top_ten(subreddit):
                 posts_titles.append(post['data']['title'])
         except Exception as e:
             pass
-            # posts_titles = None
 
         if posts_titles:
             for title in posts_titles:
                 print(title)
+        else:
+            print(None)
     else:
         print(None)
