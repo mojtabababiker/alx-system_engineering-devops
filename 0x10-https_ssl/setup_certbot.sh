d998d7d6eaafd33ea7c6c50deb18cab74ce2768b
mojtabababiker <mojmohammad98@gmail.com>
2024-03-11 19:44:59 +0200

    Add automated script to install certbot and generate ssl certificate

---
+++ 0x10-https_ssl/setup_certbot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# an automated script to install the certbot software
+# and create a ssl certificate
+# this code requier snap preinstalled
+
+# remove any preinstallde certbot
+apt-get remove certbot &> /dev/null
+
+# install the certbot
+snap install --classic certbot
+
+# create a link for the certbot on the usr/bin/certbot directory
+ln -s /snap/bin/certbot /usr/bin/certbot
+
+# stop the haproxy process to create the ssl cert
+service haproxy stop
+
+# create the ssl cert
+certbot certonly --standalone > cert_files_path
+
